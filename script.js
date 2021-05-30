0bf6e4714a1a68c8209e7f1e59829ecdee7714a9
fawasam <fawasam5@gmail.com>
2021-05-30 18:16:43 +0530

    resposive full animation web

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  const menuToggle = document.querySelector(".menuToggle");
+  menuToggle.classList.toggle("active");
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("active");
+}
